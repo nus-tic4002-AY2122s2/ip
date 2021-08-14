50b4ff930f1d21de83d62b00087b30d9eeeb6b5c
kaydenkhor <k-denkhor@live.com>
2021-08-14 21:45:17 +0800

    Level-2: Add List done by E0373956, A0194133J,  Khor Kee Yong. Refactor a little code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Duke {
     private static String line = "_______________________________________\n"; // To shift to UI class
+    private static ArrayList<String> list = new ArrayList<String>();
     public static void main(String[] args) {
         greet();
-        echo();
+        while (process(read()));
         exit();
     }
     private static void greet(){                                          // To shift to UI class
@@ -14,21 +17,53 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String greet = " Hello! I'm Duke \n What can I do for you?\n";
-        System.out.println(logo + line + greet + line);
+        dukeReply(logo + greet);
     }
 
     private static void exit(){                                                 // To shift to UI class
         String bye = "Bye. Hope to see you again soon!\n";
-        System.out.println(line + bye + line);
+        dukeReply(bye);
     }
 
-    private static void echo(){
-        while (true) {
+
+
+    private static String parse(String userInput){
+        String command = userInput.toLowerCase();
+        return command;
+    }
+
+    private static String read(){
             Scanner in = new Scanner(System.in);
-            String userInput = in.nextLine();
-            if (userInput.equalsIgnoreCase("bye"))
-                return;
-            System.out.println(line + userInput + "\n" + line);
+            String userInput = parse(in.nextLine());
+            return userInput;
+    }
+
+    private static void addToList (String userInput){
+        list.add((list.size()+1) + "." + userInput + "\n");
+        dukeReply("added: " + userInput + "\n");
+    }
+
+    private static void showList (){
+        String taskList = new String();
+        for (String task : list)
+            taskList = taskList.concat(task);
+        dukeReply(taskList);
+    }
+
+    private static void dukeReply (String reply){
+        System.out.println(line + reply + line);
+    }
+
+    private static boolean process(String userInput){
+        switch(userInput) {
+            case "bye":
+                return false;
+            case "list":
+                showList();
+                return true;
+            default:
+                addToList(userInput);
+                return true;
         }
     }
 }
