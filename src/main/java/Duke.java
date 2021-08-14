6285cedaef75a2031cba8becc2cbeb91bb09f8c3
kaydenkhor <k-denkhor@live.com>
2021-08-14 17:12:29 +0800

    Level-0: Greet done by E0373956, A0194133J,  Khor Kee Yong

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
+        initialize();
+    }
+    private static void initialize(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String line = "_______________________________________\n";
+        String greet = " Hello! I', Duke \n What can I do for you?\n";
+        String bye = "Bye. Hope to see you again soon!\n";
+        System.out.println(logo + line + greet + line + bye + line);
     }
 }
