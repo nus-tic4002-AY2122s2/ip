d31eced3368e78b03836941a01e2acf734439e9b
kaydenkhor <k-denkhor@live.com>
2021-08-14 23:17:52 +0800

    Level-3: Mark Task Done done by E0373956, A0194133J,  Khor Kee Yong. Added a new task class to handle all task object. Include command identifier. Haven't consider exception. Pure FR

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static String line = "_______________________________________\n"; // To shift to UI class
-    private static ArrayList<String> list = new ArrayList<String>();
+    private static ArrayList<Task> list = new ArrayList<Task>();
     public static void main(String[] args) {
         greet();
         while (process(read()));
@@ -25,45 +25,69 @@ public class Duke {
         dukeReply(bye);
     }
 
-
-
-    private static String parse(String userInput){
-        String command = userInput.toLowerCase();
+    private static String parse(String userInput){                              // To shift to parser class
+        String command = userInput.toLowerCase().trim();
         return command;
     }
 
-    private static String read(){
+    private static String read(){                                           // To shift to read class
             Scanner in = new Scanner(System.in);
             String userInput = parse(in.nextLine());
             return userInput;
     }
 
     private static void addToList (String userInput){
-        list.add((list.size()+1) + "." + userInput + "\n");
+        Task newTask = new Task(userInput);
+        list.add(newTask);
         dukeReply("added: " + userInput + "\n");
     }
 
-    private static void showList (){
+    private static void showList(){
         String taskList = new String();
-        for (String task : list)
-            taskList = taskList.concat(task);
+        int counter = 1;
+        for (Task task : list){
+            taskList = taskList.concat(counter + "." + task.getFullStatus());
+            counter++;
+        }
         dukeReply(taskList);
+    }
+
+    private static void markDone(String userInput){
+        String taskNumber = userInput.substring(userInput.indexOf("done")+5);
+        list.get(Integer.parseInt(taskNumber) - 1).isDone = true;
+        dukeReply("Nice! I've marked this task as done: \n" + list.get(Integer.parseInt(taskNumber) - 1).getFullStatus());
     }
 
     private static void dukeReply (String reply){
         System.out.println(line + reply + line);
     }
 
+    private static String commandIdentifier(String userInput){
+        if (userInput.contains("done"))  //priority 1
+            return "done";
+        else if (userInput.contains("list")) // priority 2
+            return "list";
+        else if (userInput.contains("bye"))
+            return "bye";
+        else
+            return "addToList";
+
+    }
+
     private static boolean process(String userInput){
-        switch(userInput) {
-            case "bye":
-                return false;
+        String command = commandIdentifier(userInput);
+        switch(command) {
             case "list":
                 showList();
                 return true;
-            default:
+            case "addToList":
                 addToList(userInput);
                 return true;
+            case "done":
+                markDone(userInput);
+                return true;
+            default:
+                return false;
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task (String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon(){
+        return (isDone ? "X" : " ");
+    }
+
+    public String getFullStatus(){
+        return ("[" + this.getStatusIcon() + "] " + this.getDescription() + "\n");
+    }
+
+    public String getDescription(){
+        return (description);
+    }
+}
