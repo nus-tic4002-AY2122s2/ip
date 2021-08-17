6e6feaff33c601adc57d308d20543e141513ae49
Gwee Yeu Chai <gweeyc@live.com.sg>
2021-08-17 21:15:05 +0800

    Duke Level-0: Greet (skeletal version)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,11 @@
 public class Duke {
+
+    static void greet(){
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("\nBye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +13,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        greet();
     }
 }
