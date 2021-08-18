ac682669c435dfd5a7b24bbc39bba689e4282c93
Gwee Yeu Chai <gweeyc@live.com.sg>
2021-08-18 23:22:57 +0800

    Duke Level-1: Greet, Echo, Exit (skeletal version)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,21 @@
-public class Duke {
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
+public class Duke {
     static void greet(){
-        System.out.println("Hello! I'm Duke");
+        System.out.println("Hello! I'm LisGenie");
         System.out.println("What can I do for you?");
-        System.out.println("\nBye. Hope to see you again soon!");
+    }
+
+    static void bye(){
+        System.out.printf("Bye. Hope to see you again soon!%n");
+    }
+
+    static void lineDraw(){
+        System.out.print("          ");
+        Stream.generate(() -> "_").limit(65).forEach(System.out::print);
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -13,7 +25,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         greet();
+
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+
+        while(true) {
+            System.out.println();
+            System.out.print("MasterOm : ");
+            input = sc.nextLine();
+            lineDraw();
+
+            if (input.equals("bye")) {
+                System.out.print("LisGenie : ");
+                bye();
+                lineDraw();
+                break;
+            } else {
+                System.out.println("LisGenie : " + input);
+                lineDraw();
+            }
+        }
+
+        sc.close();
     }
 }
