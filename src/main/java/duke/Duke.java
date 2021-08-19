72d22873ddc3fa879f2179725cafd7b82ca81441
Ruiting1 <ruiting245831717@gmail.com>
2021-08-19 20:32:41 +0800

    level0

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,3 +1,7 @@
+package main.java.duke;
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +10,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String separateLine="____________________________________________________";
+        System.out.println(separateLine);
+        System.out.println("Hello! I am Duke\nWhat can I do for you?");
+        System.out.println(separateLine);
+        System.out.println("Bye. Hope to see you again soon!");
+
+
     }
 }
