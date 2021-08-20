36afb6537fad3668a27aad8ea0c1b1ee9dfc6557
Yralle Lesly <yrallelesly@google.com>
2021-08-20 05:45:32 +0000

    Test Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+	while(true) {
+		System.out.println("Input Test\n");
+		Scanner in = new Scanner(System.in);
+		String input = in.nextLine();
+		System.out.println("You Entered : " + input );
+	}
     }
 }
