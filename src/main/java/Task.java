3d114ebee3767112c6097c47f8c48f1b21c22dc7
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-08-20 21:05:46 +0800

    Add constructor

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,4 +2,9 @@ public class Task {
     // Variables
     private String taskName;
     private boolean isDone = false;
+
+    // Constructor
+    public Task(String taskName) {
+        this.taskName = taskName;
+    }
 }
