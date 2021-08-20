a1d1f648f822dfb57f7c1ff3f0ce66404e8735ba
jr-mojito <chunyg335@gmail.com>
2021-08-20 19:45:54 +0800

    Initial Commit. Greet.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("-------------------------------");
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+
+        System.out.println("-------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-------------------------------");
     }
 }
