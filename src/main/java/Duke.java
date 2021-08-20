12b582cd178c49096681915b83be04816b0eb423
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-08-20 20:39:39 +0800

    Add comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        // Variables
         String horizLine = "____________________________________________________________";
 
+        // Init
         System.out.println("Hello! I'm Jarvis");
         System.out.println("What can I do for you?");
         System.out.println(horizLine);
 
+        // Read user input
         Scanner userInput = new Scanner(System.in);
         String inputTxt = userInput.nextLine();
         while (!inputTxt.equals("bye")) {
@@ -15,6 +18,8 @@ public class Duke {
             System.out.println(horizLine);
             inputTxt = userInput.nextLine();
         }
+
+        // Finalise
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizLine);
     }
