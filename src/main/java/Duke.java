dfe8e18095991d6ba9c2bce00c9ba7ac3c36a6ba
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-08-20 20:12:05 +0800

    Change chatbot name

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo);
-
         String horizLine = "____________________________________________________________";
+
         System.out.println(horizLine);
-        System.out.println("Hello! I'm Duke");
+        System.out.println("Hello! I'm Jarvis");
         System.out.println("What can I do for you?");
         System.out.println(horizLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizLine);
+        //System.out.println("Bye. Hope to see you again soon!");
+        //System.out.println(horizLine);
     }
 }
