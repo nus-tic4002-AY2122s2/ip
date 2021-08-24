6b1b3d760d37013bdb4d11419e740ce5d519d9d3
zcnmashleu95 <zcnmashleu@gmail.com>
2021-08-25 00:30:51 +0800

    Finished Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,23 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String greeting = " Hello! I'm Duke\n" + " What can I do for you?";
         String bye = "Bye. Hope to see you again soon!";
         System.out.println(greeting);
+
+        boolean running = true;
+        String input;
+        Scanner sc = new Scanner(System.in);
+        while(running){
+            input = sc.nextLine();
+            if(input.matches("bye")){
+                break;
+            }
+            System.out.println(input);
+        }
+
         System.out.println(bye);
+
     }
 }
