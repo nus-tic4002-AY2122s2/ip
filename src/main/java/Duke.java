1e637d2b93b36acc5af877f98fe46c486f502c49
jr-mojito <chunyg335@gmail.com>
2021-08-26 15:03:12 +0800

    Added Add and List Command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -11,21 +14,29 @@ public class Duke {
         System.out.println("-------------------------------");
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?");
-
-
         System.out.println("-------------------------------");
 
         String echo = " ";
+        ArrayList<String> addedList = new ArrayList<>();
 
         Scanner input = new Scanner(System.in);
         while (!echo.equals("bye")) {
             echo = input.nextLine();
-            if(!echo.equals("bye")){
+            if(!echo.equals("bye") && !echo.equals("list")){
+                addedList.add(echo);
                 System.out.println("-------------------------------");
                 System.out.println("    " + echo);
                 System.out.println("-------------------------------");
             }
-
+            if(echo.equals("list")){
+                int index = 0;
+                System.out.println("-------------------------------");
+                Iterator itr = addedList.iterator();
+                while (itr.hasNext()){
+                    System.out.println("    " + ++index +". " + itr.next());
+                }
+                System.out.println("-------------------------------");
+            }
         }
 
         System.out.println("-------------------------------");
