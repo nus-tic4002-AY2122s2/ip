498eec03d1478d72f44d84220d9e4cf453c60c94
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-08-26 22:28:24 +0800

    Add by variable to Deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,7 +1,17 @@
 public class Deadline extends Task {
+    // Variables
+    protected String by;
+
     // Constructor
-    public Deadline(String taskName) {
+    public Deadline(String taskName, String by) {
         super(taskName);
         this.prefix = 'D';
+        this.by = by;
+    }
+
+    // Getter
+    @Override
+    public String getTask() {
+        return (super.getTask() + " (by: " + by + ")");
     }
 }
