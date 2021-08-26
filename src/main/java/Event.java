76278c23fef86079e3f9d5e81844a2270410951a
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-08-26 22:33:35 +0800

    Add at variable to Event class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,7 +1,17 @@
 public class Event extends Task {
+    // Variables
+    protected String at;
+
     // Constructor
-    public Event(String taskName) {
+    public Event(String taskName, String at) {
         super(taskName);
         this.prefix = 'E';
+        this.at = at;
+    }
+
+    // Getter
+    @Override
+    public String getTask() {
+        return (super.getTask() + " (at: " + at + ")");
     }
 }
