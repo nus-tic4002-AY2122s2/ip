5ffb79c735145550d3f17b29b8c1d1daae152dbe
opkko <1313opkko@gmail.com>
2021-08-29 19:37:01 +0800

    "Level 1. Greet, Echo, Exit"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,94 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    static final String line = "    ____________________________________________________________";
+
+
+
+    public static void printLines(String k){
+        System.out.println(k);
+    }
+    public static ArrayList<String> list = new ArrayList<>();  // ArrayList to store userInputs
+    //public static int idx = 0;
+
+    public static void Greet(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        printLines(line);
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        printLines(line);
+    }
+
+    public static String echo(){
+        String userInput;
+        Scanner scan = new Scanner( System.in );
+        userInput = scan.nextLine();
+
+
+//        System.out.println(userInput);
+//        printLines(line);
+//        System.out.println("     " + userInput);
+//        printLines(line);
+        if(!userInput.equals("list"))
+            list.add(userInput);
+
+        return userInput;
+    }
+
+    public static boolean exit(String userInput){
+        if(userInput.equals("list")){
+            displayList(userInput);
+        }
+        if(userInput.equals("bye")){
+            System.out.println(userInput);
+            printLines(line);
+            //System.out.println("     " + userInput);
+            System.out.println("     Bye. Hope to see you again soon!");
+            printLines(line);
+            return false;
+        }
+        else {
+            if(!userInput.equals("list")){
+                System.out.println(userInput);
+                printLines(line);
+                System.out.println("     " + userInput);
+//                System.out.println("     added: " + userInput);
+                printLines(line);
+            }
+        }
+
+        return true;
+    }
+
+    public static void displayList(String input){
+        int idx = 1;
+        if(input.equals("list")){
+            System.out.println(input);
+            printLines(line);
+            for(int i = 0; i < list.size(); i++){
+                System.out.println(idx + ". " + list.get(i));
+                idx++;
+            }
+        }
+        printLines(line);
+    }
+    public static void main(String[] args) {
+        Greet();
+        while(exit(echo()));
 
     }
 }
+
+
+
+
+
+
+
+
