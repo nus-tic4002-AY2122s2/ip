b72f6bde9b537c79653a70fe016da410fe10b594
YangShuogeng <54273624+YangShuogeng@users.noreply.github.com>
2021-08-30 21:10:01 +0800

    Change the Task class and methods in the class to abstract

--- src/main/java/task_classes/Task.java
+++ src/main/java/task_classes/Task.java
@@ -1,6 +1,6 @@
 package task_classes;
 
-public class Task {
+public abstract class Task {
     private String description;
     private boolean isDone;
     protected String type;
@@ -15,32 +15,24 @@ public class Task {
      *
      * @return status icon of the task
      */
-    public String getStatusIcon(){
-        return (isDone ? "X" : " ");
-    }
+    public abstract String getStatusIcon(); //{ return (isDone ? "X" : " ");}
 
     /**
      * To get the description of the task
      *
      * @return the description of the task
      */
-    public String getDescription(){
-        return this.description;
-    }
+    public abstract String getDescription(); //{ return this.description; }
 
     /**
      * To get the status of the task
      */
-    public void markAsDone(){
-        this.isDone = true;
-    }
+    public abstract void markAsDone(); //{ this.isDone = true; }
 
     /**
      * To get the type of the task
      *
      * @return the type of the task
      */
-    public String getType(){
-        return this.type;
-    }
+    public abstract String getType(); //{ return this.type; }
 }
