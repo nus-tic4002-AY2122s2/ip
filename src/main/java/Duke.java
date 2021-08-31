a66bf617a84cc3b7318f7fad9d133c96c2e88782
lowyanshuang <lowyanshuang@gmail.com>
2021-08-31 22:36:35 +0800

    branch-Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,8 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-    //public static ArrayList<String> list = new ArrayList<>();
-    //public static ArrayList<Boolean> mark = new ArrayList<>();
     public static ArrayList<Task> taskList = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -36,46 +31,100 @@ public class Duke {
             case "bye":
                 System.out.println("Bye. Hope to see you again soon!");
                 return false;
+
             case "list":
                 printList();
                 return true;
+
             case "done":
-                int index = Integer.valueOf(splitStr[1]) - 1;
-                System.out.println("Nice! I've marked this task as done: \n" +
-                        "       [X] " + taskList.get(index).getDescription());
-                //mark.set(index, true);
-                taskList.get(index).setMark(true);
-                return true;
+                try {
+                    int index = Integer.valueOf(splitStr[1]) - 1;
+                    if (!userInput.equals("")) {
+                        System.out.println("Nice! I've marked this task as done: \n" +
+                                "       [X] " + taskList.get(index).getDescription());
+                        //mark.set(index, true);
+                        taskList.get(index).setMark(true);
+                        return true;
+                    } else {
+                        System.out.println("☹ OOPS!!! There isn't a task <empty>.");
+                        return false;
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("☹ OOPS!!! Please try again. Bye!");
+                    return false;
+                }
+
             case "todo":
-                userInput = userInput.replace("todo ","");
-                Task newTask = new Task(userInput, false, 'T', "");
-                taskList.add(newTask);
-                //newTask.setDescription(userInput);
-                System.out.println("Got it. I've added this task: \n" +
-                     "      [T][ ] " + userInput + "\n" +
-                             "Now you have " + taskList.size() + " tasks in the list.");
-                return true;
+                try {
+                    userInput = userInput.replace("todo ","");
+                    if(!userInput.equals("") && !userInput.equals("todo")){
+                        Task newTask = new Task(userInput, false, 'T', "");
+                        taskList.add(newTask);
+                        //newTask.setDescription(userInput);
+                        System.out.println("Got it. I've added this task: \n" +
+                                "      [T][ ] " + userInput + "\n" +
+                                "Now you have " + taskList.size() + " tasks in the list.");
+                        return true;
+                    }
+                    else {
+                        System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                        return false;
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("☹ OOPS!!! Please try again. Bye!");
+                    return false;
+                }
+
             case "deadline":
-                userInput = userInput.replace("deadline ","");
-                System.out.println(userInput);
-                String[] deadline = userInput.split("/");
-                Task newTask1 = new Task(deadline[0], false, 'D', deadline[1]);
-                taskList.add(newTask1);
-                System.out.println("Got it. I've added this task: \n" +
-                        "      [D][ ] " + deadline[0] + "(" + deadline[1] + ")\n" +
-                        "Now you have " + taskList.size() + " tasks in the list.");
-                return true;
+                try {
+                    userInput = userInput.replace("deadline ","");
+                    //System.out.println(userInput);
+                    if(!userInput.equals("") && !userInput.equals("deadline")){
+                        String[] deadline = userInput.split("/");
+                        Task newTask1 = new Task(deadline[0], false, 'D', deadline[1]);
+                        taskList.add(newTask1);
+                        System.out.println("Got it. I've added this task: \n" +
+                                "      [D][ ] " + deadline[0] + "(" + deadline[1] + ")\n" +
+                                "Now you have " + taskList.size() + " tasks in the list.");
+                        return true;
+                    }
+                    else {
+                        System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+                        return false;
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("☹ OOPS!!! Please try again. Bye!");
+                    return false;
+                }
+
             case "event":
-                userInput = userInput.replace("event ","");
-                String[] event = userInput.split("/");
-                Task newTask2 = new Task(event[0], false, 'E', event[1]);
-                taskList.add(newTask2);
-                System.out.println("Got it. I've added this task: \n" +
-                        "      [E][ ] " + event[0] + "(" + event[1] + ")\n" +
-                        "Now you have " + taskList.size() + " tasks in the list.");
-                return true;
+                try {
+                    userInput = userInput.replace("event ","");
+                    if(!userInput.equals("") && !userInput.equals("event")){
+                        String[] event = userInput.split("/");
+                        Task newTask2 = new Task(event[0], false, 'E', event[1]);
+                        taskList.add(newTask2);
+                        System.out.println("Got it. I've added this task: \n" +
+                                "      [E][ ] " + event[0] + "(" + event[1] + ")\n" +
+                                "Now you have " + taskList.size() + " tasks in the list.");
+                        return true;
+                    }
+                    else {
+                        System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                        return false;
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("☹ OOPS!!! Please try again. Bye!");
+                    return false;
+                }
+
             default:
-                return true;
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                return false;
         }
     }
 
