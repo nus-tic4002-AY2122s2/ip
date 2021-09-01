3ba9ce1e600eeb74f12bf53f502f9ba20a65e8f9
Ivan Ho <ivanhlb@hotmail.com>
2021-09-02 01:09:30 +0800

    Level already implemented as of TIC2002. fixed string description for MissingParamException that was newly added.

--- src/main/java/exception/DukeMissingParamException.java
+++ src/main/java/exception/DukeMissingParamException.java
@@ -3,7 +3,7 @@ package exception;
 public class DukeMissingParamException extends DukeException {
     private String message;
     public DukeMissingParamException(String command) {
-        this.message = String.format("OOPS!!! The description of %s cannot be empty.", command);
+        this.message = String.format("OOPS!!! The parameter of %s cannot be empty.", command);
     }
     @Override
     public String getMessage() {
