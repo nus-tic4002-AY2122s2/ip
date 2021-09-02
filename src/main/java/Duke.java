4c60631f0323b0134f5cdbe7e3f520bc2959c1e5
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-09-03 00:03:04 +0800

    Refactor with isAddTask()

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Duke {
     // Variables
     static final String HORIZ_LINE = "____________________________________________________________";
+    static final String CMD_TODO = "todo";
+    static final String CMD_DEADLINE = "deadline";
+    static final String CMD_EVENT = "event";
 
     // Methods
     private static void initApp() {
@@ -23,39 +26,47 @@ public class Duke {
         }
     }
 
+    private static boolean isAddTask(String inputTxt) {
+        return (inputTxt.startsWith(CMD_TODO) || inputTxt.startsWith(CMD_DEADLINE) || inputTxt.startsWith(CMD_EVENT));
+    }
+
     private static void runApp(Task[] tasks) {
         Scanner userInput = new Scanner(System.in);
         String inputTxt = userInput.nextLine();
         for (int i = 0; i < tasks.length; i++) {
-            if (inputTxt.equals("bye")) {
-                break;
-            } else if (inputTxt.equals("list")) {
-                printList(tasks, i);
-                i--;
-            } else if (inputTxt.startsWith("done")) {
-                int idx = Integer.parseInt(inputTxt.split(" ")[1]) - 1;
-                if (idx < i) {
-                    tasks[idx].setDone();
-                    System.out.println("Nice! I've marked this as done:");
-                    System.out.println(tasks[idx].getTask());
-                } else {
-                    System.out.println("Task out of limit");
+            if (isAddTask(inputTxt)) {
+                if (inputTxt.startsWith(CMD_TODO)) {
+                    tasks[i] = new Todo(inputTxt.substring(5));
+                    System.out.println("added: " + tasks[i].getTask());
+                } else if (inputTxt.startsWith(CMD_DEADLINE)) {
+                    String[] deadlineSplit = inputTxt.substring(9).split("/by");
+                    tasks[i] = new Deadline(deadlineSplit[0].trim(), deadlineSplit[1].trim());
+                    System.out.println("added: " + tasks[i].getTask());
+                } else if (inputTxt.startsWith(CMD_EVENT)) {
+                    String[] eventSplit = inputTxt.substring(6).split("/at");
+                    tasks[i] = new Event(eventSplit[0].trim(), eventSplit[1].trim());
+                    System.out.println("added: " + tasks[i].getTask());
                 }
-                i--;
-            } else if (inputTxt.startsWith("todo")) {
-                tasks[i] = new Todo(inputTxt.substring(5));
-                System.out.println("added: " + tasks[i].getTask());
-            } else if (inputTxt.startsWith("deadline")) {
-                String[] deadlineSplit = inputTxt.substring(9).split("/by");
-                tasks[i] = new Deadline(deadlineSplit[0].trim(), deadlineSplit[1].trim());
-                System.out.println("added: " + tasks[i].getTask());
-            } else if (inputTxt.startsWith("event")) {
-                String[] eventSplit = inputTxt.substring(6).split("/at");
-                tasks[i] = new Event(eventSplit[0].trim(), eventSplit[1].trim());
-                System.out.println("added: " + tasks[i].getTask());
             } else {
-                System.out.println("Invalid input");
-                i--;
+                if (inputTxt.equals("bye")) {
+                    break;
+                } else if (inputTxt.equals("list")) {
+                    printList(tasks, i);
+                    i--;
+                } else if (inputTxt.startsWith("done")) {
+                    int idx = Integer.parseInt(inputTxt.split(" ")[1]) - 1;
+                    if (idx < i) {
+                        tasks[idx].setDone();
+                        System.out.println("Nice! I've marked this as done:");
+                        System.out.println(tasks[idx].getTask());
+                    } else {
+                        System.out.println("Task out of limit");
+                    }
+                    i--;
+                } else {
+                    System.out.println("Invalid input");
+                    i--;
+                }
             }
 
             System.out.println("Total tasks: " + (i + 1));
