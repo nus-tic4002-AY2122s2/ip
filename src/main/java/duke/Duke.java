6df00beccc58e24484a0eba80165449deedb1171
Joseph <ariesyang92@gmail.com>
2021-09-05 19:28:27 +0800

    Level 5. Handle Errors

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,6 +1,6 @@
 import duke.dukeTask.*;
+import duke.dukeException.*;
 import java.util.Scanner;
-
 
 public class Duke {
     public static void main(String[] args) {
@@ -19,7 +19,12 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         Task[] taskList = new Task[100];
         int counter = 1;
-
+        // any other user input
+        String[] userInput = null;
+        String command;
+        String input;
+        String description = "";
+        String date = "";
         // user input loops
         while (true) {
             line = sc.nextLine().trim();
@@ -29,54 +34,73 @@ public class Duke {
                 sc.close();
                 break;
             }else{
-                // any other user input
-                String[] userInput = null;
-                String command;
-                String input;
-                String description = "";
-                String date = "";
                 // check for user command and input
-                if(line.indexOf(" ") > 0){
-                    userInput = line.split(" ",2);
-                    command = userInput[0].trim();
-                    input = userInput[1].trim();
-                    if(command.equals("todo")){
-                        todoFunction(taskList, input , counter);
-                        addTaskPrint(taskList,counter);
-                        counter++;
-                    }else if(command.equals("done")){
-                        int listLocation = Integer.valueOf(input);
-                        doneFunction(taskList, listLocation);
-                        donePrint();
-                    }else if(command.equals("deadline")){
-                        description = input.substring(0, input.indexOf("/by")-1);
-                        date = input.substring(input.indexOf("/by")+3, input.length());
-                        deadlineFunction(taskList, description, date, counter);
-                        addTaskPrint(taskList,counter);
-                        counter++;
-                    }else if(command.contains("event")) {
-                        description = input.substring(0, input.indexOf("/at") - 1);
-                        date = input.substring(input.indexOf("/at") + 3, input.length());
-                        eventFunction(taskList, description, date, counter);
-                        addTaskPrint(taskList,counter);
-                        counter++;
-                    }else{
+                userInput = line.split(" ",2);
+                command = userInput[0].trim().toLowerCase();
+                switch(command){
+                    case "todo":
+                        try{
+                            if(userInput[1].trim() == ""){
+                                throw new DukeException();
+                            }
+                            todoFunction(taskList, userInput[1].trim(), counter);
+                            addTaskPrint(taskList,counter);
+                        }catch(IndexOutOfBoundsException | DukeException e){
+                             System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                        }
+                        break;
+                    case "done":
+                        try{
+                            if(userInput[1].trim() == ""){
+                                throw new DukeException();
+                            }
+                            doneFunction(taskList, Integer.valueOf(userInput[1].trim()));
+                            donePrint();
+                        }catch (IndexOutOfBoundsException | DukeException e){
+                            System.out.println("☹ OOPS!!! Please enter an index.");
+                        }
+                        catch (NullPointerException e){
+                            System.out.println(" ☹ OOPS!!! The index you have key in dose not exist.");
+                        }
+                        break;
+                    case "deadline":
+                        try{
+                            if(userInput[1].trim() == ""){
+                                throw new DukeException();
+                            }
+                            description = userInput[1].trim().substring(0, userInput[1].trim().indexOf("/by")-1);
+                            date = userInput[1].trim().substring(userInput[1].trim().indexOf("/by")+3, userInput[1].trim().length());
+                            deadlineFunction(taskList, description, date, counter);
+                            addTaskPrint(taskList,counter);
+                        }catch(IndexOutOfBoundsException | DukeException e){
+                            System.out.println("☹ OOPS!!! Please check deadline command that you have key in is in correct format.");
+                        }
+                        break;
+                    case "event":
+                        try{
+                            if(userInput[1].trim() == ""){
+                                throw new DukeException();
+                            }
+                            description = userInput[1].trim().substring(0, userInput[1].trim().indexOf("/at") - 1);
+                            date = userInput[1].trim().substring(userInput[1].trim().indexOf("/at") + 3, userInput[1].trim().length());
+                            eventFunction(taskList, description, date, counter);
+                            addTaskPrint(taskList,counter);
+                        }catch(IndexOutOfBoundsException | DukeException e){
+                            System.out.println("☹ OOPS!!! Please check event command that you have key in is in correct format.");
+                        }
+                        break;
+                    case "list":
+                        printListFunction(taskList, counter);
+                        break;
+                    default:
                         wrongCommand();
-                    }
-                }else if(line.isEmpty()) {
-                    // empty user input
-                    emptyInput();
-                }else if(line.equals("list")) {
-                    System.out.println("Here are the tasks in your list:\n");
-                    printListFunction(taskList, counter);
-                }else{
-                    wrongInput();
                 }
             }
         }
     }
 
     private static void printListFunction(Task[] taskList, int counter){
+        System.out.println("Here are the tasks in your list:");
         for(int i = 1; i < counter; i++){
             System.out.println(i + "." +  taskList[i].toString());
         }
@@ -92,6 +116,7 @@ public class Duke {
         Task newTask = new Todo(description);
         //newTask.description = description;
         taskList[counter] = newTask;
+        counter++;
         return taskList;
     }
 
@@ -99,6 +124,7 @@ public class Duke {
         Task newTask = new Event(description, date);
         //newTask.description = description;
         taskList[counter] = newTask;
+        counter++;
         return taskList;
     }
 
@@ -106,19 +132,12 @@ public class Duke {
         Task newTask = new Deadline(description, date);
         //newTask.description = description;
         taskList[counter] = newTask;
+        counter++;
         return taskList;
-    }
-
-    private static void emptyInput() {
-        System.out.println("You have enter an empty string!!!");
     }
 
     private static void wrongCommand(){
         System.out.print("You have enter an invalid command!!!");
-    }
-
-    private static void wrongInput(){
-        System.out.println("You have enter an invalid input!!!");
     }
 
     private static void donePrint(){
