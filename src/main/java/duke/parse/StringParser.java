a3b30992295bfc740cf54dd6dc77cd602b02bf49
l-shihao <e0166067@u.nus.edu>
2021-09-10 18:31:22 +0800

    Branch for Level-7

--- src/main/java/duke/parse/StringParser.java
+++ src/main/java/duke/parse/StringParser.java
@@ -1,5 +1,7 @@
 package duke.parse;
 
+import duke.task.*;
+
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
@@ -17,7 +19,6 @@ public class StringParser {
         support.firePropertyChange("keyArgs", null, keyArgs);
     }
 
-
     public static String[] removeFirst(String[] xxs) {
         String[] xs = new String[xxs.length - 1];
         for (int i = 1; i < xxs.length; i++) {
@@ -32,6 +33,35 @@ public class StringParser {
             arg += word + " ";
         }
         return arg.strip();
+    }
+
+    public static Task stringToTask(String line) {
+        String[] parts = line.split("]", 3);
+        Boolean isDone = !parts[1].equals("[ ")? true : false;
+
+        Task res;
+        String[] args;
+
+        switch (parts[0]) {
+            case "[T":
+                res = new Todo(parts[2]);
+                break;
+            case "[E":
+                args = parts[2].split("/at");
+                res = new Event(args[0], args[1]);
+                break;
+            case "[D":
+                args = parts[2].split("/by");
+                res = new Deadline(args[0], args[1]);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + parts[0]);
+        }
+
+        if (isDone) {
+            res.markDone();
+        }
+        return res;
     }
 
     public void addPropertyChangeListener(PropertyChangeListener pcl) {
