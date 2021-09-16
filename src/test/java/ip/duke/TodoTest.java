6525c2237a3845a21a990fbd6fd10053df2e9eaf
Gwee Yeu Chai <gweeyc@live.com.sg>
2021-09-16 13:09:12 +0800

    A-JUnit: Add JUnit Tests
    Done 3 tests for Todo class.
    
    Remove the Wrapper.java as not needed.

---
+++ src/test/java/ip/duke/TodoTest.java
@@ -0,0 +1,29 @@
+package ip.duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+
+    @Test
+    public void Desc_Test(){
+        Todo task = new Todo("return a book");
+        assertEquals("return a book", task.getDescription());
+
+    }
+
+    @Test
+    public void SetId_Test(){
+        Todo task = new Todo("read another book");
+        assertEquals('T', task.getId());
+    }
+
+    @Test
+    public void ToFileString_Test(){
+        Todo task = new Todo("borrow a book");
+        task.setDone();
+        assertEquals("T : 1 : borrow a book",task.toFileString());
+    }
+
+}
