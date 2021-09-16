34603bd7f485c71972cfff7f1b34f76a8f027e95
yongming <zhuangyongming1900@gmail.com>
2021-09-17 01:45:04 +0800

    Week 4, Level 5, remove special character in error message and update test cases

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -1,12 +1,12 @@
 package constant;
 
 public class ErrorMessage {
-    public static final String EMPTY_TODO = "☹ OOPS!!! The description of a todo cannot be empty.";
-    public static final String EMPTY_DEADLINE = "☹ OOPS!!! The description of a deadline cannot be empty.";
-    public static final String EMPTY_EVENT = "☹ OOPS!!! The description of a event cannot be empty.";
-    public static final String EMPTY_TASK_NUMBER = "☹ OOPS!!! The task number cannot be empty.";
-    public static final String INVALID_TASK_NUMBER = "☹ OOPS!!! The task number is invalid.";
-    public static final String INVALID_DEADLINE = "☹ OOPS!!! The deadline description is invalid, missing '/by'.";
-    public static final String INVALID_EVENT = "☹ OOPS!!! The event description is invalid, missing '/at'.";
-    public static final String INVALID_COMMAND = "☹ OOPS!!! I'm sorry, but I don't know what that means.";
+    public static final String EMPTY_TODO = "OOPS!!! The description of a todo cannot be empty.";
+    public static final String EMPTY_DEADLINE = "OOPS!!! The description of a deadline cannot be empty.";
+    public static final String EMPTY_EVENT = "OOPS!!! The description of a event cannot be empty.";
+    public static final String EMPTY_TASK_NUMBER = "OOPS!!! The task number cannot be empty.";
+    public static final String INVALID_TASK_NUMBER = "OOPS!!! The task number is invalid.";
+    public static final String INVALID_DEADLINE = "OOPS!!! The deadline description is invalid, missing '/by'.";
+    public static final String INVALID_EVENT = "OOPS!!! The event description is invalid, missing '/at'.";
+    public static final String INVALID_COMMAND = "OOPS!!! I'm sorry, but I don't know what that means.";
 }
