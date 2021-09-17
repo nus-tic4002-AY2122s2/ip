4d6999a7a41901279a7c447a54e2afd2dc6e3629
kaydenkhor <k-denkhor@live.com>
2021-09-17 13:00:47 +0800

    Added A-JUnit and implement A-MoreOOP

--- src/test/java/UiTest.java
+++ src/test/java/UiTest.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UiTest {
+
+    @Test
+    public void UiTest() {
+        Ui testUi = new Ui();
+        assertEquals("\t________________________________________________________________\n\t", testUi.showLine());
+    }
 }
+
