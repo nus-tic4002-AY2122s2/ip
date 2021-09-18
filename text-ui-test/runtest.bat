678e11f15dc24c2d74389cd631eb86fac12f6757
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-09-18 19:04:27 +0800

    Delete test data folder if exist

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,6 +6,9 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
+REM delete data folder if exist
+if exist data rmdir data /s /q
+
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\edu\nus\duke\Main.java
 IF ERRORLEVEL 1 (
