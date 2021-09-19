cda8a9e527a0c39f17af5454311f56c898295185
IcyNoPeople <leowjunhui2@hotmail.com>
2021-09-19 22:25:27 +0800

    k

--- src/main/java/duke/task/ToDo.java
+++ src/main/java/duke/task/ToDo.java
@@ -1,30 +1,17 @@
 package duke.task;
 
 
-/**
- * The ToDo class for user to store their To Do task
- */
+
 public class ToDo extends Task {
 
-<<<<<<< HEAD
 
 
 
-=======
-    /**
-     * Constructs and store the task that the user input
-     * @param thingsToDo The task that the user input to do
-     */
->>>>>>> branch-A-JavaDoc
     public ToDo(String thingsToDo) {
         super(thingsToDo);
         taskType = TaskType.TODO;
     }
 
-    /**
-     * When this function is called, it will return the task type with the task that the user input and whether it is done or not
-     * @return the task in String
-     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
