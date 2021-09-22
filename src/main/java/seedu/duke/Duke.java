6fb7c9b75b8ecf23b00d8034999cc9b9c168b566
lowyanshuang <lowyanshuang@gmail.com>
2021-09-22 21:07:09 +0800

    Include greeting and exit message

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -6,16 +6,38 @@ public class Duke {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
+    public static final String errorUnknown = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+    public static void main(String[] args) {
+        String logo = "    ___     ______  \n"
+                + "   /   \\    |     | \n"
+                + "  /  _  \\   |_____| \n"
+                + " /  |_|  \\  |  \\    \n"
+                + "/         \\ |   \\   \n";
+        System.out.println("Hi I’m AIR REC\n" + logo);
+        System.out.println("I can help you save all the flight details!");
+        System.out.println("How can I assist you today?");
+
+        //Scanner in = new Scanner(System.in);
+        //System.out.println("Hello " + in.nextLine());
+
+        while (echo());
+    }
+
+
+    public static boolean echo() {
+
+        String userInput;
+        Scanner scan = new Scanner(System.in);
+        userInput = scan.nextLine();
+
+        switch (userInput) {
+            case "bye":
+                System.out.println("Bye. Your tasks has been saved.\n Hope to see you again soon!");
+                return false;
+            default:
+                System.out.println(errorUnknown);
+                return false;
+        }
     }
 }
