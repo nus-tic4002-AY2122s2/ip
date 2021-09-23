34f45cb5a72d63577394ceafe13786df73824d1c
chenghuiyuan <huiyuanhyhy@gmail.com>
2021-09-23 14:51:37 +0800

    Add JUnit tests

---
+++ src/test/java/task/TodoTest.java
@@ -0,0 +1,16 @@
+package task;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TodoTest {
+    @Test
+    @DisplayName("Inserting Test Case 1 ")
+    void TestToString() {
+        Todo todo = new Todo("To complete Todo task");
+        assertEquals("[T][✘]To complete Todo task", todo.toString());
+    }
+
+}
