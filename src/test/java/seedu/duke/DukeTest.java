e3b988a894248c37a8d7ed870eedb2627c9d9758
kaydenkhor <k-denkhor@live.com>
2021-09-25 21:25:03 +0800

    Added Flight Class. Constructor only. Added Flight Creation Test case to test file.

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -4,9 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+
 class DukeTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
+    }
+
+    @Test
+    public void flightCreationTask(){
+        Flight testFlight = new Flight("Singapore", "Australia","2021-12-31 0800", 500);
     }
 }
