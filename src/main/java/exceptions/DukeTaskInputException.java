5ebdecc45883d67af82637e8ce0998df09f38d90
YangShuogeng <54273624+YangShuogeng@users.noreply.github.com>
2022-01-22 15:17:38 +0800

    Use checkstyle function to correct some codes

--- src/main/java/exceptions/DukeTaskInputException.java
+++ src/main/java/exceptions/DukeTaskInputException.java
@@ -1,7 +1,5 @@
 package exceptions;
 
-import ui.Ui;
-
 public class DukeTaskInputException extends Exception {
 
     private static String errorType;
@@ -10,7 +8,7 @@ public class DukeTaskInputException extends Exception {
      * To initialize DukeTaskInputException
      * @param errorTypeInput the error type
      */
-    public DukeTaskInputException(String errorTypeInput){
+    public DukeTaskInputException(String errorTypeInput) {
         errorType = errorTypeInput;
     }
 
@@ -19,14 +17,15 @@ public class DukeTaskInputException extends Exception {
      *
      * @return the error type
      */
-    public static String getErrorType(){
+    public static String getErrorType() {
         return errorType;
     }
 
     /**
      * To print the error message if user try to reach any task in empty task list
      */
-    public static void toPrintListIsEmptyError(){
+    public static void toPrintListIsEmptyError() {
+
         System.out.println("     ☹ OOPS!!! The Task List is empty.");
     }
 
@@ -34,13 +33,14 @@ public class DukeTaskInputException extends Exception {
      * The method to print command creation error
      */
     public static void toPrintCommandCreateError() {
+
         System.out.println("     ☹ OOPS!!! The Command you just input was in wrong format.");
     }
 
     /**
      * To print the error message for the wrong input format
      */
-    public static void formatWrong(){
+    public static void formatWrong() {
         //Ui.toPrintSeparateLine();
         System.out.println("     \u2639 OOPS!!! The input format wrong, please try again. :-(");
     }
@@ -48,7 +48,7 @@ public class DukeTaskInputException extends Exception {
     /**
      * To print the error message if the first word of user input is not in the detection list:
      */
-    public static void invalidFirstWordInput(){
+    public static void invalidFirstWordInput() {
         //Ui.toPrintSeparateLine();
         System.out.println("     \u2639" + " OOPS!!! I'm sorry, but I don't know what that means :-(");
         //Ui.toPrintSeparateLine();
