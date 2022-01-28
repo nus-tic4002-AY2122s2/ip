6351e9b4f0ac58ea2855a7efe95a4160e4db5e6f
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-01-28 16:57:05 +0800

    Add stage properties

--- src/main/java/edu/nus/duke/Main.java
+++ src/main/java/edu/nus/duke/Main.java
@@ -16,6 +16,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duke");
+            stage.setResizable(false);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
