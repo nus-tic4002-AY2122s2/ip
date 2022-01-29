57dd04b07614029eec608d3e5e25a4ddb505eb2f
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-01-29 18:13:42 +0800

    Auto scroll ScrollPane after user input

--- src/main/java/edu/nus/duke/Main.java
+++ src/main/java/edu/nus/duke/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -14,6 +15,9 @@ public class Main extends Application {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             VBox vBox = fxmlLoader.load();
+            ScrollPane scrollPane = (ScrollPane) vBox.getChildren().get(0);
+            VBox dialogContainer = (VBox) scrollPane.getContent();
+            dialogContainer.heightProperty().addListener((observable -> scrollPane.setVvalue(1.0)));
             Scene scene = new Scene(vBox);
             stage.setScene(scene);
             stage.setTitle("Duke");
