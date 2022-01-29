7d9cef9377d820fb26021cae8cbb69283f0126ea
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-01-29 13:34:58 +0800

    Add user input (label) via button click

--- src/main/java/edu/nus/duke/view/MainWindow.java
+++ src/main/java/edu/nus/duke/view/MainWindow.java
@@ -1,6 +1,18 @@
 package edu.nus.duke.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class MainWindow extends VBox {
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private void handleUserInput() {
+        dialogContainer.getChildren().add(new Label(userInput.getText()));
+        userInput.clear();
+    }
 }
