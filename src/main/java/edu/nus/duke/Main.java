10b2bdf34400d241e2be25d5a4ebdc011758a770
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-01-29 18:21:01 +0800

    Refactor auto scroll ScrollPane with initScrollPane()

--- src/main/java/edu/nus/duke/Main.java
+++ src/main/java/edu/nus/duke/Main.java
@@ -2,10 +2,10 @@ package edu.nus.duke;
 
 import java.io.IOException;
 
+import edu.nus.duke.view.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,9 +15,7 @@ public class Main extends Application {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             VBox vBox = fxmlLoader.load();
-            ScrollPane scrollPane = (ScrollPane) vBox.getChildren().get(0);
-            VBox dialogContainer = (VBox) scrollPane.getContent();
-            dialogContainer.heightProperty().addListener((observable -> scrollPane.setVvalue(1.0)));
+            fxmlLoader.<MainWindow>getController().initScrollPane();
             Scene scene = new Scene(vBox);
             stage.setScene(scene);
             stage.setTitle("Duke");
--- src/main/java/edu/nus/duke/view/MainWindow.java
+++ src/main/java/edu/nus/duke/view/MainWindow.java
@@ -1,17 +1,24 @@
 package edu.nus.duke.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 
 public class MainWindow extends VBox {
     @FXML
+    private ScrollPane scrollPane;
+    @FXML
     private VBox dialogContainer;
     @FXML
     private TextField userInput;
 
     private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+
+    public void initScrollPane() {
+        dialogContainer.heightProperty().addListener((observable -> scrollPane.setVvalue(1.0)));
+    }
 
     @FXML
     private void handleUserInput() {
