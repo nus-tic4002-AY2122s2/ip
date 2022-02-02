456a9623b0b6423a3804754f88de2ad9f3498f4b
chenghuiyuan <huiyuanhyhy@gmail.com>
2022-02-02 14:59:49 +0800

    Fix assertion

--- src/test/java/task/DeadlineTest.java
+++ src/test/java/task/DeadlineTest.java
@@ -1,18 +1,16 @@
 package task;
 
-import junit.framework.TestCase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DeadlineTest {
-    private TestCase Assertions;
-
     @Test
     @DisplayName("Inserting Test Case 2 /by 2021-10-01 23:59")
     void TestToString() {
         String dateTime = "2021-10-01 23:59";
         Deadline deadline = new Deadline("To complete Deadline task", dateTime);
-        Assertions.assertEquals("[D][✘]To complete Deadline task (by: 2021-10-01 23:59)", deadline.toString());
+        assertEquals("[D][✘]To complete Deadline task (by: 2021-10-01 23:59)", deadline.toString());
     }
 
 }
