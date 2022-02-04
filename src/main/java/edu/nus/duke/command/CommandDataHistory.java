0af966a8a68925063157859e7b72eb251caf1125
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-02-04 13:44:52 +0800

    Init CommandDataHistory class

---
+++ src/main/java/edu/nus/duke/command/CommandDataHistory.java
@@ -0,0 +1,9 @@
+package edu.nus.duke.command;
+
+import java.util.ArrayList;
+
+import edu.nus.duke.task.TaskList;
+
+public class CommandDataHistory {
+    private ArrayList<TaskList> taskLists;
+}
