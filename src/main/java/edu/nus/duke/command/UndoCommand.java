d947ba752aa9686bd05ad97f98ba44ea3377cb06
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2022-02-04 13:25:14 +0800

    Add CMD String

--- src/main/java/edu/nus/duke/command/UndoCommand.java
+++ src/main/java/edu/nus/duke/command/UndoCommand.java
@@ -3,6 +3,8 @@ package edu.nus.duke.command;
 import edu.nus.duke.task.TaskList;
 
 public class UndoCommand extends Command {
+    public static final String CMD = "undo";
+
     public CommandResult run(TaskList taskList) {
         return (new CommandResult("Undo success", false));
     }
