59bf9a77114dd3d90257d63d9470c154a1a1d123
Yralle Lesly <yralleljg@gmail.com>
2022-02-20 03:06:32 +0800

    Add JavaFX Library

--- src/main/java/main/Duke.java
+++ src/main/java/main/Duke.java
@@ -7,11 +7,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 //*
 // Individual Project for TIC4002 2022 Jan-May
 //
 // *//
-public class Duke {
+public class Duke extends Application {
 
     public static ArrayList<Task> Tasks = new ArrayList<>();
 
@@ -34,5 +39,19 @@ public class Duke {
 
         storage.save();
 
+    }
+
+    // Writing First Program JavaFX
+    //
+    //
+    //
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
