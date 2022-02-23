c93f83df002657cb49b5e420d5c82d7bf41c287f
Yralle Lesly <yralleljg@gmail.com>
2022-02-23 13:11:11 +0800

    Code Improvements

--- src/main/java/main/Duke.java
+++ src/main/java/main/Duke.java
@@ -4,8 +4,10 @@ import javafx.stage.Stage;
 import main.parsers.ParserText;
 import main.taskLists.Task;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -44,9 +46,22 @@ public class Duke {
      * Replace this stub with your completed method.
      */
     public String getResponse(String input, Stage stage) {
-        boolean isExit;
         String response = "";
-        response = "TOOLBOX";
+        ParserText res = new ParserText();
+
+        ByteArrayOutputStream strBuffer = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(strBuffer);
+        PrintStream old = System.out;
+        System.setOut(ps);
+        try {
+            res.parsetext(input);
+        } catch (IOException | DukeException e) {
+
+        }
+        System.out.flush();
+        System.setOut(old);
+        response = strBuffer.toString();
+
         return response;
     }
 
--- src/main/java/main/UI.java
+++ src/main/java/main/UI.java
@@ -42,13 +42,13 @@ public class UI {
     }
 
     static void line() {
-        System.out.println("\t____________________________________________________________");
+        System.out.println("\t__________________________________________________________");
     }
 
     public static void bye() {
-        System.out.println("\t____________________________________________________________");
+        System.out.println("\t__________________________________________________________");
         System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println("\t____________________________________________________________");
+        System.out.println("\t__________________________________________________________");
     }
 
     public static void echo(String input) {
